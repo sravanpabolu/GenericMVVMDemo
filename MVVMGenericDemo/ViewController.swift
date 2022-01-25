86f9d290020002170749ed6d53757f7f71c50a3e
Sravan <sravanpabolu@gmail.com>
2022-01-25 12:22:24 +0530

    Misc

--- MVVMGenericDemo/ViewController.swift
+++ MVVMGenericDemo/ViewController.swift
@@ -2,18 +2,52 @@
 //  ViewController.swift
 //  MVVMGenericDemo
 //
-//  Created by Hello World on 25/01/22.
+//  Created by Sravan on 21/01/22.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    let userViewModel = UserViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initializeCompletion()
+        getUsers()
     }
+}
 
+extension ViewController {
+    func getUsers() {
+        userViewModel.getUsers()
+    }
+    
+    fileprivate func initializeCompletion() {
+        userViewModel.completion = { [weak self] in
+            guard let self = self else { return }
+            
+            if let error = self.userViewModel.errorString {
+                //SHOW ALERT
+                self.showErrorAlert(message: error)
+                return
+            }
+            
+            guard let users = self.userViewModel.users else {
+                self.showErrorAlert(message: "Users Data error")
+                return
+            }
+            
+            print("Users: \n \(users)")
+        }
+    }
+}
 
+extension ViewController {
+    func showErrorAlert(message: String) {
+        //SHOW ALERT
+        print("Error: \(message)")
+    }
 }
 
